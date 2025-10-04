3c90c4989a0fe88eb5f10eced59270592994db40
sniper2002praveen <sniper2002praveen@gmail.com>
2025-10-04 16:55:12 +0000

    nn

--- server/routes/index.js
+++ server/routes/index.js
@@ -22,7 +22,7 @@ router.get("/ping", (req, res) => {
 
 router.get("/status", (req, res) => {
   console.log("Server is running")
-  res.status(200).send("server is running");
+  res.status(200).send("server is running 1");
 });
 
 module.exports = router;
